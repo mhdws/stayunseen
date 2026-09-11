a79d06ca842fbfbb0e6d1b5db8270a05f1f65231
Jayson Doctolero <66167665+mhdws@users.noreply.github.com>
2026-09-12 03:34:21 +0800

    Add files via upload

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./index.html", "./assets/app.js"],
+  content: ["./*.html", "./assets/app.js"],
   theme: {
     extend: {
       colors: {
